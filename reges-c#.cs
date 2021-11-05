d5f8b3be3a0db7b9476277153cef074a97cb59f8
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-11-05 20:23:48 -0300

    Regex C# - regex no mundo .Net, alterando o formato de uma data, alterando o separador.

---
+++ reges-c#.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System. Threading.Tasks;
+
+namespace ConsoleApplication3 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alvo = "11a22b33c";
+            string pattern = @"(\d\d)(\w)";
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(alvo); 
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Groups[0].Value);
+                Console.WriteLine(match.Groups[1].Value);
+                Console.WriteLine(match.Groups[2].Value);
+                Console.WriteLine(match.Index);
+                Console.WriteLine(match.Length); 
+            }   
+        }
+    }
+}
